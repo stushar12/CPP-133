f1cbd298764d2f1b1207be48bfcb67f96dd2070b
Tushar Saxena <stushar345@gmail.com>
2021-01-16 12:39:34 +0530

    Add files via upload

---
+++ one.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+	string s="abc";
+	void write(string s,int n)
+	{
+		int i=0;
+		while(n>0)
+		{
+			(n&1)?cout<<s[i]: cout<<"";
+			i++;
+			n=n>>1;
+		}
+		cout<<"\n";
+	}
+
+int main() {
+	
+
+	int n=s.size();
+
+	int range=(1<<n)-1;
+
+	for(int i=0;i<=range;i++)
+	{
+		write(s,i);
+	}
+	
+	return 0;
+}
