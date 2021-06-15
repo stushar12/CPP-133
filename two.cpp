dd0b71b311eefe0560429255778be34ce03f3493
Tushar Saxena <stushar345@gmail.com>
2021-06-15 20:32:54 +0530

    Create two.cpp

---
+++ two.cpp
@@ -0,0 +1,26 @@
+// s="abc"
+// "","a","b","c","ab","ac","bc","abc"
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void subsequence(string input,string output)
+{
+
+if(input.length()==0)
+{
+//if(output.length()>0)                                 if we want to print only non empty subsequence
+cout<<output<<" "<<output.length()<<endl;
+return;
+}
+
+subsequence(input.substr(1),output);            //not including the variable and passing the rest input
+
+subsequence(input.substr(1),output+input[0]);       //including the variable and passing the rest input
+
+}
+
+int main()
+{
+   subsequence("abc","");
+}
