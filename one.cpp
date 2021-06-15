2bc07fbf3704a484b7952c238c4ab57fcfa9bfc1
Tushar Saxena <stushar345@gmail.com>
2021-06-15 20:40:05 +0530

    Using bit manipulation

--- one.cpp
+++ one.cpp
@@ -1,25 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
-	string s="abc";
-	void write(string s,int n)
-	{
-		int i=0;
-		while(n>0)
-		{
-			(n&1)?cout<<s[i]: cout<<"";
-			i++;
-			n=n>>1;
-		}
-		cout<<"\n";
-	}
 
-int main() {
-	
+void write(string s,int n)
+{
+    int i=0;
+    while(n>0)
+    {
+        (n&1)?cout<<s[i]: cout<<"";
+        i++;
+        n=n>>1;
+    }
+    cout<<"\n";
+}
 
+int main() 
+{
+    string s="abc";
 	int n=s.size();
-
 	int range=(1<<n)-1;
-
 	for(int i=0;i<=range;i++)
 	{
 		write(s,i);
